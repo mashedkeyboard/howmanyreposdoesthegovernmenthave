0874b11f7780ce2862e1c787c058090d9832a9fd
Curtis Parfitt-Ford <curtis@mashedkeyboard.me>
2017-06-25 12:07:52 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# howmanyreposdoesthegovernmenthave
-How many GitHub repos does the UK Government have?
+# How many GitHub repos does the UK Government have?
+This project counts the number of GitHub repositories that the Government Digital Service currently has. It's really quite simple. It's just a number on a page with some JavaScript.
---
+++ gds-repo-count.js
@@ -0,0 +1,54 @@
+var orglist = ['alphagov','gds-operations','gds-attic'];
+var repocounts = [];
+var urlbase = 'https://api.github.com/orgs/';
+var repoCounter = document.getElementById("count");
+var orgsIncluded = document.getElementById("orgsIncluded");
+
+orgsIncluded.innerHTML = "Includes ";
+repoCounter.innerHTML = "Loading...";
+repoCounter.className = "big";
+
+for (var org in orglist) {
+	var isLast = (org == (orglist.length - 1));
+	if (org != 0 && !isLast) {
+		orgsIncluded.innerHTML += ', ';
+	}
+	else if (isLast) {
+		orgsIncluded.innerHTML += ' and ';
+	}
+	orgsIncluded.innerHTML += '<a href="https://github.com/' + orglist[org] + '">' + orglist[org] + '</a>';
+	callApi(urlbase + orglist[org], setCount);
+}
+waitForCount();
+
+function callApi(url, fnc) {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() { 
+	if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+	    fnc(xmlHttp.responseText);
+	}
+	xmlHttp.open("GET", url, true);
+	xmlHttp.send();
+}
+
+function setCount(response) {
+	ghrs = JSON.parse(response);
+	repocounts.push(ghrs.public_repos);
+}
+
+function waitForCount() {
+	if (orglist.length == repocounts.length) {
+		setResponse();
+	}
+	else {
+		setTimeout(waitForCount, 100);
+	}
+}
+
+function setResponse() {
+	var repocount = 0;
+	for (var count in repocounts) {
+		repocount += repocounts[count];
+	}
+	repoCounter.innerHTML = repocount;
+}
